532eef8b38fdf1914171422d7b26fe6bee28af35
akrolayer <almagestf532@gmail.com>
2020-02-21 23:51:00 +0900

    Slider

--- PhotoList/PhotoList/ContentView.swift
+++ PhotoList/PhotoList/ContentView.swift
@@ -16,8 +16,50 @@ struct ContentView: View {
     @State var kosu:Int = 0
     let tanka = 240
     let tax = 0.1
-    
+    @State var volume:Double = 0.0
+    @State var R:Double = 0
+    @State var G:Double = 0
+    @State var B:Double = 0
     var body: some View {
+        VStack(alignment: .center){
+            Circle()
+                .frame(width: 100,height: 100)
+                .padding()
+                .foregroundColor(Color(red: R/255, green: G/255, blue:B/255))
+            HStack{
+                Circle()
+                    .foregroundColor(.red)
+                    .frame(width:20, height:20)
+                Text(String(Int(R))).frame(width:40)
+                Slider(value: $R, in: 0...255).frame(width:200)
+            }
+            HStack{
+                Circle()
+                    .foregroundColor(.green)
+                    .frame(width:20,height: 20)
+                Text(String(Int(G))).frame(width:40)
+                Slider(value: $G, in:0...255).frame(width:200)
+            }
+            HStack{
+                Circle()
+                    .foregroundColor(.blue)
+                    .frame(width:20,height:20)
+                Text(String(Int(B))).frame(width:40)
+                Slider(value: $B, in:0...255).frame(width:200)
+            }
+        }
+        /*
+        HStack{
+            Text("\(format(volume))").frame(width: 100)
+            HStack{
+                Image(systemName: "speaker.slash").imageScale(.large)
+                Slider(value: $volume)
+                Image(systemName: "speaker.3").imageScale(.large)
+            }
+            .frame(width: 200)
+        }
+ */
+        /*
         VStack(alignment: .leading, spacing: 20){
     Text("５個ずつ３０個まで、１個\(self.tanka)円").font(.headline)
             
@@ -36,6 +78,7 @@ struct ContentView: View {
             Text("料金:\(calc(self.kosu))円[税込]").underline()
             
         }
+ */
         /*
         VStack{
             Toggle(isOn: $iLike) {
@@ -147,6 +190,10 @@ struct ContentView: View {
         print(num)
         let result = Double(price) * (1+self.tax)
         return Int(result)
+    }
+    func format(_ num:Double)->String{
+        let result = String(round(num*100)/100)
+        return result
     }
 }
 
